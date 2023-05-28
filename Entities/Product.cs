df0e5ce9a6c38412a9495a46a53a171ad81a8928
Nadir Abrar <nabrar@avanceon.ae>
2023-05-28 13:54:03 +0500

    bug fixes : 5_27_2023

---
+++ Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace URLEntryMVC.Entities
+{
+    public class Product
+    {
+        public string? ProductCode { get; set; }
+        public string? ProductName { get; set; }
+        public double TotalRemaining { get; set; }
+        public double DailyUsageRate { get; set; }
+        public int RemainingDays { get; set; }
+    }
+}
